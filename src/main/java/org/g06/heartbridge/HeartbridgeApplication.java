840e5a9662da5d5cc79ea1fff1bebedb43f6225d
ltdingyilang <2641251178@qq.com>
2025-03-06 13:05:32 +0800

    创建初始项目

---
+++ src/main/java/org/g06/heartbridge/HeartbridgeApplication.java
@@ -0,0 +1,13 @@
+package org.g06.heartbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartbridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeartbridgeApplication.class, args);
+    }
+
+}
