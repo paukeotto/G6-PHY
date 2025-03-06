9422a96427895d9eb734d6a2d37cfdb76cf89980
ltdingyilang <2641251178@qq.com>
2025-03-06 13:46:54 +0800

    名字换驼峰式

--- src/main/java/org/g06/heartbridge/HeartBridgeApplication.java
+++ src/main/java/org/g06/heartbridge/HeartBridgeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HeartbridgeApplication {
+public class HeartBridgeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HeartbridgeApplication.class, args);
+        SpringApplication.run(HeartBridgeApplication.class, args);
     }
 
 }
